aaaaae30900b4c507f5cc7c18107e2f19d4b9f35
Jaini Vora <vorajaini@gmail.com>
2017-11-11 17:09:04 -0600

    Added add topic POST method

--- src/main/java/io/springbootcourse/topic/TopicService.java
+++ src/main/java/io/springbootcourse/topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.springbootcourse.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,9 +9,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 
-	private List<Topic> topics = Arrays.asList(new Topic("spring", "Spring Framework", "Spring description"),
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(new Topic("spring", "Spring Framework", "Spring description"),
 			new Topic("java", "Core Java", "Core Java description"),
-			new Topic("javascript", "JavaScript", "JavaScript Description"));
+			new Topic("javascript", "JavaScript", "JavaScript Description")
+			));
 
 	public List<Topic> getAllTopics() {
 		return topics;
@@ -18,5 +20,9 @@ public class TopicService {
 
 	public Topic getTopic(String id) {
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 }
