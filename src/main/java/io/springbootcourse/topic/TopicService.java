fe077811bfe1dd3e75798506fb6fd577736ea633
Jaini Vora <vorajaini@gmail.com>
2017-11-11 16:43:30 -0600

    Added retrieval of Individual topic functionality

--- src/main/java/io/springbootcourse/topic/TopicService.java
+++ src/main/java/io/springbootcourse/topic/TopicService.java
@@ -7,15 +7,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	
-	private List<Topic> topics = Arrays.asList(
-			new Topic("spring","Spring Framework","Spring description"),
-			new Topic("java","Core Java","Core Java description"),
-			new Topic("javascript","JavaScript","JavaScript Description")
-			);
-	
 
-	public List<Topic> getAllTopics(){
+	private List<Topic> topics = Arrays.asList(new Topic("spring", "Spring Framework", "Spring description"),
+			new Topic("java", "Core Java", "Core Java description"),
+			new Topic("javascript", "JavaScript", "JavaScript Description"));
+
+	public List<Topic> getAllTopics() {
 		return topics;
+	}
+
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
 	}
 }
